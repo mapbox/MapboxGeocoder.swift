2c2a827283d03fbd075719b7d158066084a3442a
Justin R. Miller <incanus@codesorcery.net>
2014-08-17 12:58:39 -0700

    Initial Commit

---
+++ Geocoder Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geocoder Example
+//
+//  Created by Justin R. Miller on 8/17/14.
+//  Copyright (c) 2014 Mapbox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
