33570eba69d58f8d9a110c85e1a85177bf94bd5c
Justin R. Miller <incanus@codesorcery.net>
2016-11-07 16:41:30 -0800

    clarify normal POI vs. landmark
    To avoid confusion, we should not list the same types of places in both
    descriptions. Also, borrowed some text from the REST API description of
    what landmarks are exactly.

--- MapboxGeocoder/MBPlacemarkScope.h
+++ MapboxGeocoder/MBPlacemarkScope.h
@@ -22,9 +22,9 @@ typedef NS_OPTIONS(NSUInteger, MBPlacemarkScope) {
     MBPlacemarkScopeNeighborhood = (1 << 7),
     /// A physical address, such as to a business or residence.
     MBPlacemarkScopeAddress = (1 << 8),
-    /// A point of interest, such as a business, school, or place of worship.
+    /// A point of interest, such as a business or school.
     MBPlacemarkScopePointOfInterest = (1 << 9),
-    /// A point of interest, such as school or place of worship -- subset of MBPlacemarkScopePointOfInterest
+    /// Subset of regular points of interest. Landmarks are particularly notable or long-lived features like parks, museums and places of worship.
     MBPlacemarkScopePointOfInterestLandmark = (1 << 10),
 
     /// All scopes.
