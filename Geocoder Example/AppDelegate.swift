7841fcb7a4a6004b06fc1ea101504a633f9a2da1
Justin R. Miller <incanus@codesorcery.net>
2016-11-10 16:20:39 -0800

    remove whitespace

--- Geocoder Example/AppDelegate.swift
+++ Geocoder Example/AppDelegate.swift
@@ -3,12 +3,12 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window!.rootViewController = ViewController(nibName: nil, bundle: nil)
         window!.makeKeyAndVisible()
-        
+
         return true
     }
 }
