70b172790348a93edea3d9737c61458d58f0ffcf
Simon Seyer <simon.seyer@gmail.com>
2016-11-18 12:49:30 +0300

    Added nil check before adding underlying error to dict
    Cherry-picked from 967218c413eb82fc4d621f196050eff974b907f9.

--- MapboxGeocoder/MBGeocoder.swift
+++ MapboxGeocoder/MBGeocoder.swift
@@ -306,7 +306,9 @@ public class Geocoder: NSObject {
             userInfo[NSLocalizedFailureReasonErrorKey] = failureReason ?? userInfo[NSLocalizedFailureReasonErrorKey] ?? NSHTTPURLResponse.localizedStringForStatusCode(error?.code ?? -1)
             userInfo[NSLocalizedRecoverySuggestionErrorKey] = recoverySuggestion ?? userInfo[NSLocalizedRecoverySuggestionErrorKey]
         }
-        userInfo[NSUnderlyingErrorKey] = error
+        if let error = error {
+            userInfo[NSUnderlyingErrorKey] = error
+        }
         return NSError(domain: error?.domain ?? MBGeocoderErrorDomain, code: error?.code ?? -1, userInfo: userInfo)
     }
 }
