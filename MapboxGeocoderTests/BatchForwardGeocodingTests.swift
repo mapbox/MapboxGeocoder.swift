8723c076a75c94b21a55bcbec6136442b166ce6d
Nadia Barbosa <nadiabarbosa@me.com>
2018-06-28 16:56:04 -0700

    Add test for valid fwd batch geocoding with multiple queries

--- MapboxGeocoderTests/BatchForwardGeocodingTests.swift
+++ MapboxGeocoderTests/BatchForwardGeocodingTests.swift
@@ -9,8 +9,8 @@ class BatchForwardGeocodingTests: XCTestCase {
         super.tearDown()
     }
     
-    func testValidForwardGeocode() {
-        let expectation = self.expectation(description: "forward batch geocode should return results")
+    func testValidForwardSingleBatchGeocode() {
+        let expectation = self.expectation(description: "forward batch geocode with single query should return results")
         
         _ = stub(condition: isHost("api.mapbox.com")
             && isPath("/geocoding/v5/mapbox.places-permanent/85+2nd+st+san+francisco.json")
@@ -28,10 +28,51 @@ class BatchForwardGeocodingTests: XCTestCase {
             let results = placemarks![0]
             let attribution = attribution![0]
         
-            XCTAssertEqual(results.count, 5, "forward batch geocode should have 5 results")
+            XCTAssertEqual(results.count, 5, "single forward batch geocode should have 5 results")
             
-            XCTAssertEqual(attribution, "NOTICE: © 2017 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service (https://www.mapbox.com/about/maps/). This response and the information it contains may not be retained.")
+            XCTAssertEqual(attribution, "© 2017 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service. (https://www.mapbox.com/about/maps/)")
             
+            expectation.fulfill()
+        }
+        XCTAssertNotNil(task)
+        
+        waitForExpectations(timeout: 1) { (error) in
+            XCTAssertNil(error, "Error: \(error!)")
+            XCTAssertEqual(task.state, URLSessionTask.State.completed)
+        }
+    }
+    
+    func testValidForwardMultipleBatchGeocode() {
+        let expectation = self.expectation(description: "forward batch geocode with multiple queries should return results")
+        
+        _ = stub(condition: isHost("api.mapbox.com")
+            && isPath("/geocoding/v5/mapbox.places-permanent/20001;20001;20001.json")
+            && containsQueryParams(["country": "us", "access_token": BogusToken])) { _ in
+                let path = Bundle(for: type(of: self)).path(forResource: "permanent_forward_multiple_valid", ofType: "json")
+                return OHHTTPStubsResponse(fileAtPath: path!, statusCode: 200, headers: ["Content-Type": "application/vnd.geo+json"])
+        }
+        
+        let geocoder = Geocoder(accessToken: BogusToken)
+        
+        let options = ForwardBatchGeocodeOptions(queries: ["20001", "20001", "20001"])
+        options.allowedISOCountryCodes = ["US"]
+        
+        let task = geocoder.batchGeocode(options) { (placemarks, attribution, error) in
+            
+            let queries = placemarks!
+            let attribution = attribution![0]
+            
+            XCTAssertEqual(queries.count, 3, "forward batch geocode should have 3 queries")
+            
+            for result in queries {
+                XCTAssertEqual(result.count, 5, "each forward batch geocode query should have 5 found results")
+                XCTAssertEqual(attribution, "© 2017 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service. (https://www.mapbox.com/about/maps/)")
+            }
+            
+            let sampleResult = queries[0][0]
+            
+            XCTAssertEqual(sampleResult.qualifiedName, "Washington, District of Columbia 20001, United States")
+
             expectation.fulfill()
         }
         XCTAssertNotNil(task)
