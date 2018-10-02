cde2e667f1b249a785212ccdc7430790f712942c
Harish Yerra <hyerra08@gmail.com>
2018-10-01 23:16:09 -0500

    Convert project to Swift 4.2.

--- Geocoder Example/AppDelegate.swift
+++ Geocoder Example/AppDelegate.swift
@@ -4,7 +4,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window!.rootViewController = ViewController(nibName: nil, bundle: nil)
         window!.makeKeyAndVisible()
