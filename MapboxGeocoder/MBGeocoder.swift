ef0293918ccb8e2a7111b73ee7cd947a7ae2cb72
席萍萍 Brook <cirpxpp@163.com>
2019-01-22 11:21:29 +0800

    another way to cast the error

--- MapboxGeocoder/MBGeocoder.swift
+++ MapboxGeocoder/MBGeocoder.swift
@@ -269,10 +269,11 @@ open class Geocoder: NSObject {
         return URLSession.shared.dataTask(with: request) { (data, response, error) in
 
             guard let data = data else { 
-                if let error = error as? NSError {
-                    errorHandler(error)
+                if let e = error as NSError? {
+                    errorHandler(e)
                 } else {
-                    errorHandler(NSError(domain: MBGeocoderErrorDomain, code: -1024, userInfo: [NSLocalizedDescriptionKey : "unexpected error", NSDebugDescriptionErrorKey : "this error happens when data task return nil data and nil error, which typically is not possible"])) 
+                    let unexpectedError = NSError(domain: MBGeocoderErrorDomain, code: -1024, userInfo: [NSLocalizedDescriptionKey : "unexpected error", NSDebugDescriptionErrorKey : "this error happens when data task return nil data and nil error, which typically is not possible"])
+                    errorHandler(unexpectedError) 
                 }
                 return
             }
